9c1b9bf9a4fcaf58697e1a4b417e35361ee88c7f
Brenda Pizano Purificati <brendapizanopurificati@gmail.com>
2026-03-06 21:31:28 -0300

    primeiro exercicio

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace exercicio1
+{
+    class exercicio1
+    {
+        static void Main()
+        {
+            double qntd_min, qntd_max, estoque;
+            string peca;
+
+            Console.WriteLine("Digite o nome da peça: ");
+            peca = Console.ReadLine();
+            Console.WriteLine("Digite a quantidade mínima para a peça:");
+            qntd_min = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite a quantidade máxima: ");
+            qntd_max = double.Parse(Console.ReadLine());
+            estoque = (qntd_min+qntd_max)/2;
+            Console.WriteLine($"O estoque médio de {peca} é: {estoque}");
+        }
+    }
+}
