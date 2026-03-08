91ecdde09980d1ef175d81d564b7c448e916e20b
Brenda Pizano Purificati <brendapizanopurificati@gmail.com>
2026-03-08 11:29:08 -0300

    Segundo exercício

---
+++ 2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exercicio2
+{
+    class Exercicio2
+    {
+        static void Main()
+        {
+            double cot_dolar = double.Parse(Console.ReadLine());
+            double val_dolar = double.Parse(Console.ReadLine());
+            double val_real = cot_dolar*val_dolar;
+            Console.WriteLine($"Valor de {val_dolar} dólares, com a cotação {cot_dolar}, em real: {val_real}");
+        }
+    }
+}
