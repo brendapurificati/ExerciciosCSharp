5ce02e04d0b5094b612c0a66a6521e9320dffd31
Brenda Pizano Purificati <brendapizanopurificati@gmail.com>
2026-03-08 11:29:41 -0300

    Segundo exercício

--- 1/obj/Debug/net10.0/1.AssemblyInfo.cs
+++ 1/obj/Debug/net10.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfd06671388995a347a8d805378bac14e5b66544")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c1b9bf9a4fcaf58697e1a4b417e35361ee88c7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
