1ebc23633f94897b47d3707703c56632acfdda83
Brenda Pizano Purificati <brendapizanopurificati@gmail.com>
2026-03-09 23:01:55 -0300

    Sétimo exercicio

---
+++ 7/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Exercicio7
+{
+    class Exercicio7
+    {
+        static void Main()
+        {
+            /*Escreva um programa que leia um número inteiro e exiba o seu módulo.
+            O módulo de um número x é:
+            x se x é maior ou igual a zero
+            x * (-1) se x é menor que zero */
+
+            Console.WriteLine("Digite um valor inteiro: ");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a<0)
+            {
+                Console.WriteLine($"O módulo de {a} é {a*(-1)}");
+            }
+            else {
+                Console.WriteLine($"O módulo de {a} é {a}");
+            }
+
+        }
+    }
+}
