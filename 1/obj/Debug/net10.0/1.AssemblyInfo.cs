7d2e5c73176c9fd57beefd5352eb45f4594355f9
Brenda Pizano Purificati <brendapizanopurificati@gmail.com>
2026-03-22 23:02:06 -0300

    .

--- 1/obj/Debug/net10.0/1.AssemblyInfo.cs
+++ 1/obj/Debug/net10.0/1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ebc23633f94897b47d3707703c56632acfdda83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c61c300fb1fffb772194bacfe63261d0dbe82072")]
 [assembly: System.Reflection.AssemblyProductAttribute("1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
